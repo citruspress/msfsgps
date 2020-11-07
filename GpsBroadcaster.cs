53544b882ff9e541094d33bfd49c6d8b346df590
Citruspress <simon.hemaker@gmail.com>
2020-11-07 14:20:57 +0100

    Initial commit

---
+++ GpsBroadcaster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MSFSGPS
+{
+    internal class GpsBroadcaster : IDisposable
+    {
+        private readonly UdpClient _udpClient = new UdpClient();
+        private readonly IPEndPoint _broadcastAddress = new IPEndPoint(IPAddress.Broadcast, 49002);
+
+        public void BroadcastPosition(Position position)
+        {
+            var altitude = Math.Round(position.Altitude, 3).ToString(CultureInfo.InvariantCulture);
+            var longitude = Math.Round(position.Longitude, 3).ToString(CultureInfo.InvariantCulture);
+            var latitude = Math.Round(position.Latitude, 3).ToString(CultureInfo.InvariantCulture);
+            var groundTrack = Math.Round(position.GroundTrack, 3).ToString(CultureInfo.InvariantCulture);
+            var speed = Math.Round(position.GroundSpeed, 3).ToString(CultureInfo.InvariantCulture);
+            byte[] sendBytes = Encoding.ASCII.GetBytes(string.Format("XGPSMSFSGPS,{0},{1},{2},{3},{4}", longitude, latitude, altitude, groundTrack, speed));
+            _udpClient.Send(sendBytes, sendBytes.Length, _broadcastAddress);
+        }
+
+        public void BroadcastAttitudeData(Attitude attitude)
+        {
+            var heading = Math.Round(attitude.Heading, 3).ToString(CultureInfo.InvariantCulture);
+            var pitch = Math.Round(attitude.Pitch, 3).ToString(CultureInfo.InvariantCulture);
+            var roll = Math.Round(attitude.Bank, 3).ToString(CultureInfo.InvariantCulture);
+            byte[] sendBytes = Encoding.ASCII.GetBytes(string.Format("XATTMSFSGPS,{0},{1},{2}", heading, pitch, roll));
+            _udpClient.Send(sendBytes, sendBytes.Length, _broadcastAddress);
+        }
+
+        public void Dispose() => _udpClient.Dispose();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using FlightSimApi;
+using System;
+using System.Threading.Tasks;
+
+namespace MSFSGPS
+{
+    internal enum Definitions
+    {
+        Attitude,
+        Position,
+    }
+
+    [SimConnectType(Definitions.Attitude, samplingFrequency: 100)]
+    internal struct Attitude
+    {
+        [SimConnectValue("PLANE HEADING DEGREES TRUE", "Degrees")]
+        public double Heading;
+
+        [SimConnectValue("PLANE PITCH DEGREES", "Degrees")]
+        public double Pitch;
+
+        [SimConnectValue("PLANE BANK DEGREES", "Degrees")]
+        public double Bank;
+    }
+
+    [SimConnectType(Definitions.Position, samplingFrequency: 1000)]
+    internal struct Position
+    {
+        [SimConnectValue("PLANE LONGITUDE", "Degrees")]
+        public double Longitude;
+
+        [SimConnectValue("PLANE LATITUDE", "Degrees")]
+        public double Latitude;
+
+        [SimConnectValue("PLANE ALTITUDE", "Meters")]
+        public double Altitude;
+
+        [SimConnectValue("GPS GROUND TRUE TRACK", "Degrees")]
+        public double GroundTrack;
+
+        [SimConnectValue("GPS GROUND SPEED")]
+        public double GroundSpeed;
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var api = new SimConnectApi();
+            using var gpsBroadcaster = new GpsBroadcaster();
+
+            api.RegisterType<Attitude>();
+            api.RegisterType<Position>();
+
+            api.RegisterHandler<Attitude>(gpsBroadcaster.BroadcastAttitudeData);
+            api.RegisterHandler<Position>(gpsBroadcaster.BroadcastPosition);
+
+            api.Start();
+
+            var lastConnectedState = false;
+
+            while (true)
+            {
+                if (lastConnectedState != api.IsConnected)
+                {
+                    lastConnectedState = api.IsConnected;
+                    Console.WriteLine($"{(api.IsConnected ? "Connected" : "Disconnected")} at {DateTime.UtcNow}");
+                }
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
